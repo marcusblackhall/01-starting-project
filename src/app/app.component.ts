becc83f22812be8782a51a5f129ace67d220d716
Marcus <marcus.blackhall@splendiddata.com>
2022-07-31 14:12:59 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AnalyticsService } from './shared/analytics.service';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@Component({
+  standalone: true,
+  imports:[WelcomeComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
+
+bootstrapApplication(AppComponent,{
+  providers:[AnalyticsService]
+})
